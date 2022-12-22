deeb05d30cd34659fe2f1c18d8ab0721ced1d05f
ayush <ayush.hakmn@gmail.com>
2022-12-22 23:28:35 +0530

    added testsets

--- fairseq/inference/compute_comet_chrf.sh
+++ fairseq/inference/compute_comet_chrf.sh
@@ -1,5 +1,5 @@
 pred_fname=$1
 ref_fname=$2
 src_fname=$3
-python comet_compute.py $src_fname $pred_fname $ref_fname >> ${pred_fname}-comet-chrf.txt
+python comet_compute.py $src_fname $pred_fname $ref_fname > ${pred_fname}-comet-chrf.txt
 python chrf_compute.py $pred_fname $ref_fname >> ${pred_fname}-comet-chrf.txt
